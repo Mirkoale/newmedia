b6a4e27bcbb90f9fc79e74ca9ae41ce648571fd8
mirkobenini <mirko.alessandro.benini@gmail.com>
2023-04-13 11:50:08 +0200

    aggiunta file

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel="stylesheet" media="screen and (min-device-width: 800px)" href="./styles/style.css">
+    <title>
+        <?= str_replace(".php", "", end(explode("/", $_SERVER['REQUEST_URI']))) ?>
+    </title>
+</head>
+
+<body>
